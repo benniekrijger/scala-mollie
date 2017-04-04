f1a0f921d29d614e40490f7e3b49317f4e46687d
benniekrijger <benniekrijger@hotmail.com>
2017-04-04 14:24:05 +0200

    Bumped version

--- build.sbt
+++ build.sbt
@@ -1,10 +1,10 @@
 name := """scala-mollie"""
 
-version := "0.11"
+version := "0.12"
 
 organization := "com.github.benniekrijger"
 
-crossScalaVersions := Seq("2.11.8", "2.12.0")
+crossScalaVersions := Seq("2.11.8", "2.12.1")
 scalaVersion := crossScalaVersions.value.head
 crossVersion := CrossVersion.binary
 
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,8 +2,8 @@ import sbt._
 
 object  Dependencies {
   object Version {
-    val akka = "2.4.14"
-    val akkaHttp = "10.0.0"
+    val akka = "2.4.17"
+    val akkaHttp = "10.0.5"
   }
 
   val common = Seq(
@@ -11,12 +11,12 @@ object  Dependencies {
     "com.typesafe.akka" %% "akka-testkit" % Version.akka % "test",
     "com.typesafe.akka" %% "akka-slf4j" % Version.akka,
     "com.typesafe.akka" %% "akka-http" % Version.akkaHttp,
-    "de.heikoseeberger" %% "akka-http-json4s" % "1.11.0",
-    "org.json4s"        %% "json4s-native" % "3.5.0",
-    "org.json4s"        %% "json4s-jackson" % "3.5.0",
-    "joda-time"         %  "joda-time" % "2.9.4",
+    "de.heikoseeberger" %% "akka-http-json4s" % "1.14.0",
+    "org.json4s"        %% "json4s-native" % "3.5.1",
+    "org.json4s"        %% "json4s-jackson" % "3.5.1",
+    "joda-time"         %  "joda-time" % "2.9.9",
     "org.joda"          %  "joda-convert" % "1.8.1",
-    "org.scalatest"     %% "scalatest" % "3.0.0" % "test",
-    "ch.qos.logback"    %  "logback-classic" % "1.1.6"
+    "org.scalatest"     %% "scalatest" % "3.0.1" % "test",
+    "ch.qos.logback"    %  "logback-classic" % "1.2.3"
   )
 }
