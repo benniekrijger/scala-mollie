fea0b1bda9ef6b4acd69b722ca6024aff3b5198b
benniekrijger <benniekrijger@hotmail.com>
2019-12-07 15:56:14 +0100

    upgrade scala and other deps, bump to 0.26

--- build.sbt
+++ build.sbt
@@ -1,6 +1,6 @@
 name := """scala-mollie"""
 
-version := "0.25"
+version := "0.26"
 
 organization := "com.github.benniekrijger"
 
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,13 +2,13 @@ import sbt._
 
 object Dependencies {
   val common = Seq(
-    "com.typesafe.akka" %% "akka-actor"       % "2.6.0",
-    "com.typesafe.akka" %% "akka-testkit"     % "2.6.0" % "test",
-    "com.typesafe.akka" %% "akka-http"        % "10.1.10",
+    "com.typesafe.akka" %% "akka-actor"       % "2.6.1",
+    "com.typesafe.akka" %% "akka-testkit"     % "2.6.1" % "test",
+    "com.typesafe.akka" %% "akka-http"        % "10.1.11",
     "de.heikoseeberger" %% "akka-http-json4s" % "1.29.1",
     "org.json4s"        %% "json4s-jackson"   % "3.6.7",
     "org.json4s"        %% "json4s-ext"       % "3.6.7",
     "joda-time"         %  "joda-time"        % "2.10.5",
-    "org.scalatest"     %% "scalatest"        % "3.0.8" % "test"
+    "org.scalatest"     %% "scalatest"        % "3.1.0" % "test"
   )
 }
